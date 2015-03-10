80092636af01ae4c685c5c45efe044d721094bbb
TomDeng <14068728@qq.com>
2015-03-10 20:58:22 +0800

    init

---
+++ java-books-example/src/main/java/com/easytoolsoft/javabooks/App.java
@@ -0,0 +1,13 @@
+package com.easytoolsoft.javabooks;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
